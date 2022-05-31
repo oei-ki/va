5aa32ddf5d8975b73ab87bdeb123e59d0bb3443e
oei-ki <90708384+oei-ki@users.noreply.github.com>
2022-05-31 17:46:26 +0900

    2022.05.31

---
+++ js/common.js
@@ -0,0 +1,11 @@
+// $(function(){
+//   $(window).scroll(function(){
+//     let sctop = $(this).scrollTop();
+//     console.log(sctop);
+//     if( sctop > 100 ){
+//       $("body #header").addClass("fixed")
+//     }else{
+//       $("body #header").removeClass("fixed")
+//     }
+//   });
+// });
