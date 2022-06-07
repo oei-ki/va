fb990b52e015ed7b45e2c3dc4db8e2511f3f5724
oei-ki <90708384+oei-ki@users.noreply.github.com>
2022-06-07 17:47:19 +0900

    2022.060.07

--- js/common.js
+++ js/common.js
@@ -1,11 +1,17 @@
-// $(function(){
-//   $(window).scroll(function(){
-//     let sctop = $(this).scrollTop();
-//     console.log(sctop);
-//     if( sctop > 100 ){
-//       $("body #header").addClass("fixed")
-//     }else{
-//       $("body #header").removeClass("fixed")
-//     }
-//   });
-// });
+$(function(){
+  $(window).scroll(function(){
+    let sctop = $(this).scrollTop();
+    console.log(sctop);
+    if( sctop > 100 ){
+      $("body #header").removeClass("header-padding")
+      $("body #header").addClass("header-sticky")
+      $(".logo img").attr('src','./img/logo-vac-nav-bk.png')
+      $("body #header .nav-wrap .hamber span").css('background','black')
+    }else if( sctop < 100 ){
+      $("body #header").addClass("header-padding")
+      $("body #header").removeClass("header-sticky")
+      $(".logo img").attr('src','./img/logo-vac-nav.png')
+      $("body #header .nav-wrap .hamber span").css('background','white')
+    }
+  });
+});
